567090de62495af315d462321893f58957734b6e
meenakshisundaram-in <109644642+meenakshisundaram-in@users.noreply.github.com>
2022-09-04 15:42:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,3 +3,11 @@ length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
 # Write your code here
+output=[]
+count=0
+for x in circular_linked_list:
+    if x not in output:
+        output.append(x)
+        count+=1
+print(count)
+print(output)
